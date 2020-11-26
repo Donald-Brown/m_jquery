c476704b8c51d831996068899db2d8bde83276ad
donald-brown <donbrown4652@gmail.com>
2020-11-25 19:15:32 -0600

    begins murachs jQuery

---
+++ README.md
@@ -0,0 +1,6 @@
+# Work on Murach's jQuery
+
+## Section 1
+
+## Chapter 1
+- web skills needed
---
+++ chapter_1/email_list/email_list.js
@@ -0,0 +1,9 @@
+
+
+
+//* testing my skills
+// window.onload = function() {
+//   document.getElementById('join_list').addEventListener('click', function() {
+//     document.getElementById('email_form').submit();
+//   });
+// }
